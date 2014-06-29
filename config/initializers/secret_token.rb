dd5784c36bc34e7abe00ad1a2c8fcad267d4e6e4
Ignasi Vegas <ignasivegas@gmail.com>
2014-06-29 15:50:14 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quotapp::Application.config.secret_token = 'bd6bde06795b30af47693c4a6b5c92cd07dc2d9a301b48e9522316ae6ff9b2e97f56f657166dfcbb892f9bbbc4f37392630a03dd13bc12810f7fe0966e34a97d'
